1017a2ce90995ec1cef24952360e6bc15deee984
O-Mike25 <75988106+O-Mike25@users.noreply.github.com>
2021-06-17 15:10:46 +0100

    moyenne des notes

--- Mike.py
+++ Mike.py
@@ -1,7 +1,7 @@
-Mike={'ps':[12,12,12] ,'C':[13,13,13],'Java':[14,14,14],'mbdd':[15,15,15]}
+Mike={'ps':[12,12,12] ,'C':[13,13,13],'java':[14,14,14],'mbdd':[15,15,15]}
 m_ps=Mike['ps'][0]+Mike['ps'][1]+Mike['ps'][2]/3
 m_C=Mike['C'][0]+Mike['C'][1]+Mike['C'][2]/3
 m_mbdd=Mike['mbdd'][0]+Mike['mbdd'][1]+Mike['mbdd'][2]/3
 m_java=Mike['java'][0]+Mike['java'][1]+Mike['java'][2]/3
-m=m_ps+m_C+m_mbdd+m_java /4
-print("la moyenne de mon dictionnaire est" , m_mbdd)
+m= (m_ps+m_C+m_mbdd+m_java) /4
+print("la moyenne de mon dictionnaire est" , m)
