5625ab6ee0b5187c566343fda76cb1d96a50a554
O-Mike25 <75988106+O-Mike25@users.noreply.github.com>
2021-06-17 15:01:21 +0100

    Moyenne des notes

---
+++ Mike.py
@@ -0,0 +1,6 @@
+Mike={'ps':[12,12,12] ,'C':[13,13,13],'Java':[14,14,14],'mbdd':[15,15,15]}
+m_ps=Mike['ps'][0]+Mike['ps'][1]+Mike['ps'][2]/3
+m_C=Mike['C'][0]+Mike['C'][1]+Mike['C'][2]/3
+m_mbdd=Mike['mbdd'][0]+Mike['mbdd'][1]+Mike['mbdd'][2]/3
+m=m_ps+m_C+m_mbdd /3
+print("la moyenne de mon dictionnaire est" , m_mbdd)
